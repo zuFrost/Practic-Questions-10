2893f75458293249f34fdb651540e942c9d1101c
Alex Frost <zufrost.a@gmail.com>
2020-05-07 11:59:12 +0300

    add GenericListStringLear

---
+++ scr/GenericListStringLear.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenericListStringLear {
+    public static void main(String[] args) {
+        List<String> parsedWords = new LinkedList<>();
+        List parsedWords2 = new LinkedList();
+    }
+}
